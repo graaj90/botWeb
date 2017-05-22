1c4aa37905d6ebf1c69c2819df37d83aba7ef554
graaj90 <rajkumar.webnexus@gmail.com>
2017-05-22 12:59:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.bc96fe00-9700-4b14-bba9-d9a5a5008723,
+    appPassword: process.env.rVqPbL82F8RRjab6n9BrNnY
 });
 
 // Listen for messages from users 
